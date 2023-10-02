41a71bdc1b84ced750924444e8f033031d452c9d
Alexandre Maurice <hizaaknewton@gmail.com>
2023-10-02 18:23:52 +0200

    Après avoir appuyé sur le bouton 'mettre en forme'

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
   public login: string = '';
@@ -12,14 +12,10 @@ export class LoginPageComponent {
 
   public signIn() {
     if (this.login === 'admin' && this.password === 'admin') {
-
       console.log('You are logged in');
       return true;
     }
     console.log('You are not logged in');
     return false;
   }
-  
 }
-
-
